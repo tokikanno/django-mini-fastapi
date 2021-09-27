ad4421d0aa5bfd865e3ebc0da8d6a6500e9c33b4
toki <toki.kanno@gmail.com>
2021-09-27 16:30:19 +0000

    use origin HTTPException

--- django_mini_fastapi/fastapi/exceptions.py
+++ django_mini_fastapi/fastapi/exceptions.py
@@ -6,19 +6,6 @@ from pydantic.error_wrappers import ErrorList
 
 # from starlette.exceptions import HTTPException as StarletteHTTPException
 
-
-# class HTTPException(StarletteHTTPException):
-class HTTPException(Exception):
-    def __init__(
-        self,
-        status_code: int,
-        detail: Any = None,
-        headers: Optional[Dict[str, Any]] = None,
-    ) -> None:
-        super().__init__(status_code=status_code, detail=detail)
-        self.headers = headers
-
-
 RequestErrorModel: Type[BaseModel] = create_model("Request")
 WebSocketErrorModel: Type[BaseModel] = create_model("WebSocket")
 
